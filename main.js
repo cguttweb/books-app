fdd449a26d71c987f4e4dc07074ba6fcbefd0617
Chloe <cguttweb@pm.me>
2023-06-16 14:26:15 +0100

    adding books to class

--- main.js
+++ main.js
@@ -12,18 +12,67 @@ document.querySelector('#app').innerHTML = `
     </a>
   </div>
 `
-const title = document.getElementById('bookTitle').value
-const author = document.getElementById('bookAuthor').value
-const format = document.getElementById('bookFormat').value
-const category = document.getElementById('category').value
-const genre = document.getElementById('genre').value
-const isbn = document.getElementById('isbn').value
-const publisher = document.getElementById('bookPublisher').value
-const yearPublished = document.getElementById('bookYearPublished').value
+
+class Book {
+  constructor(
+    title,
+    author,
+    format,
+    category,
+    genre,
+    isbn,
+    published,
+    yearPublished
+  ) {
+    this._title = title
+    this._author = author
+    this._format = format
+    this._category = category
+    this._genre = genre
+    this._isbn = isbn
+    this._published = published
+    this._yearPublished = yearPublished
+  }
+
+  static clearBookFields() {
+    document.querySelector('#bookTitle').value
+    document.querySelector('#bookAuthor').value
+    document.querySelector('#bookFormat').value
+    document.querySelector('#bookCategory').value
+    document.querySelector('#bookGenre').value
+    document.querySelector('#bookYearPublished').value
+    document.querySelector('#bookIsbn').value
+    document.querySelector('#bookPublisher').value
+  }
+}
 
 const addBookBtn = document.getElementById('addBook')
-let Books = []
+let myBooks = []
 
 addBookBtn.addEventListener('click', e => {
   e.preventDefault()
+  const title = document.getElementById('bookTitle').value
+  const author = document.getElementById('bookAuthor').value
+  const format = document.getElementById('bookFormat').value
+  const category = document.getElementById('bookCategory').value
+  const genre = document.getElementById('bookGenre').value
+  const isbn = document.getElementById('bookIsbn').value
+  const publisher = document.getElementById('bookPublisher').value
+  const yearPublished = document.getElementById('bookYearPublished').value
+
+  const book = new Book(
+    title,
+    author,
+    format,
+    category,
+    genre,
+    isbn,
+    publisher,
+    yearPublished
+  )
+
+  myBooks.push(book)
+
+  console.log(myBooks)
+  Book.clearBookFields()
 })
