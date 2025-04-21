8a4711ed449bea68726396aa15e877a9eeb4510d
Chloe <cguttweb@pm.me>
2025-04-21 22:27:50 +0100

    starting on form to add books

--- src/main.js
+++ src/main.js
@@ -1,24 +1,44 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <h1>Books App</h1>
+    <form id="book-form">
+      <label for="title">Title:</label>
+      <input type="text" id="title" name="title" required />
+
+      <label for="author">Author:</label>
+      <input type="text" id="author" name="author" required />
+
+      <label for="genre">Genre:</label>
+      <input type="text" id="genre" name="genre" required />
+
+      <label for="year">Year:</label>
+      <input type="number" id="year" name="year" required />
+      <label for="publisher">Publisher:</label>
+      <input type="text" id="publisher" name="publisher" required />
+
+      <label for="published_date">Published Date:</label>
+      <input type="date" id="published_date" name="published_date" required />
+
+      <label for="isbn">ISBN (if known):</label>
+      <input type="text" id="isbn" name="isbn" />
+      
+      <label for="format">Format:</label>
+      <input type="text" id="format" name="format" required />
+
+      <button type="submit">Add Book</button>
+    </form>
+    <footer>
+      <a href="https://vite.dev" target="_blank">
+        <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+        <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+      </a>
+    </footer>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
