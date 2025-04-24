45573aa7f6b6f75f238cd4e3a7ac41a7f4cdd06e
Chloe <cguttweb@pm.me>
2025-04-24 19:30:52 +0100

    making a start on using classes and retrieving values from form to store in an array

---
+++ src/books.js
@@ -0,0 +1,13 @@
+class Book {
+  constructor(title, author, genre, publisher, published_date, format, isbn) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.publisher = publisher;
+    this.published_date = published_date;
+    this.format = format;
+    this.isbn = isbn;
+  }
+}
+
+export default Book;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
+import Book from './books.js'
+
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -16,20 +18,21 @@ document.querySelector('#app').innerHTML = `
       <input type="text" id="genre" name="genre" required />
 
       <label for="year">Year:</label>
-      <input type="number" id="year" name="year" required />
+      <input type="number" id="year" name="year"  />
+
       <label for="publisher">Publisher:</label>
-      <input type="text" id="publisher" name="publisher" required />
+      <input type="text" id="publisher" name="publisher" />
 
       <label for="published_date">Published Date:</label>
-      <input type="date" id="published_date" name="published_date" required />
+      <input type="date" id="published_date" name="published_date" />
 
       <label for="isbn">ISBN (if known):</label>
       <input type="text" id="isbn" name="isbn" />
       
       <label for="format">Format:</label>
-      <input type="text" id="format" name="format" required />
+      <input type="text" id="format" name="format" />
 
-      <button type="submit">Add Book</button>
+      <button id="addBook" type="submit">Add Book</button>
     </form>
     <footer>
       <a href="https://vite.dev" target="_blank">
@@ -41,4 +44,38 @@ document.querySelector('#app').innerHTML = `
     </footer>
   </div>
 `
+let books = []
 
+function storeBook(event) {
+  event.preventDefault()
+  const form = event.target
+  const title = document.querySelector('#title').value
+  const author = document.querySelector('#author').value  
+  const genre = document.querySelector('#genre').value
+  const publisher = document.querySelector('#publisher').value
+  const published_date = document.querySelector('#published_date').value
+  const format = document.querySelector('#format').value
+  const isbn = document.querySelector('#isbn').value
+  const year = document.querySelector('#year').value
+  if (year < 1900 || year > new Date().getFullYear()) {
+    alert('Please enter a valid year between 1900 and the current year.')
+    return
+  }
+
+  const book = new Book(
+    title,
+    author,
+    genre,
+    publisher,
+    published_date,
+    format,
+    isbn,
+    year
+  )
+  console.log('Book added:', book)
+  books.push(book)
+  console.log('Books array:', books)
+}
+
+const submitButton = document.querySelector('#addBook')
+submitButton.addEventListener('click', storeBook)
