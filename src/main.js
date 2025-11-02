9501237f203f6c661aad245e5f9658e440b35530
Chloe <cguttweb@pm.me>
2025-11-02 23:11:03 +0000

    initial commit of initial working example to insert a single book

---
+++ src/main.js
@@ -0,0 +1,74 @@
+import "./style.css"
+import javascriptLogo from "./javascript.svg"
+import viteLogo from "/vite.svg"
+import { supabase } from "./supabase.js"
+
+const footer = document.querySelector("footer")
+
+footer.innerHTML = `
+<p>Built by <a href="https://github.com/cguttweb">Chloe</a>with 
+<a href="https://vite.dev" target="_blank">
+<img src="${viteLogo}" class="logo" alt="Vite logo" width="30" />
+</a> <span>and</span>
+<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+<img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" width="30" />
+</a>
+</p>
+`
+
+const form = document.querySelector("#book-form")
+const bookList = document.querySelector("#books-table tbody")
+
+async function loadBooks() {
+  const { data, error } = await supabase
+    .from("books")
+    .select("*")
+    .order("title")
+  if (error) return console.error(error)
+
+  bookList.innerHTML = data
+    .map(
+      (book) => `
+      <tr>
+        <td>${book.title}</td>
+        <td>${book.author || ""}</td>
+        <td>${book.genre || ""}</td>
+        <td>${book.format || ""}</td>
+        <td>${book.publisher || ""}</td>
+        <td>${book.yearPublished || ""}</td>
+        <td>${book.isbn || ""}</td>
+        <td>${book.read ? "✅" : "X"}</td>
+        <td>${book.rating || ""}</td>
+        <td>${book.notes || ""}</td>
+      </tr>
+    `
+    )
+    .join("")
+}
+
+loadBooks()
+
+async function addBooks() {
+  const { data, error } = await supabase.from("books").insert()
+  console.log(data)
+}
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault()
+  const formData = new FormData(form)
+
+  const book = Object.fromEntries(formData.entries())
+  book.read = formData.get("read") ? true : false
+
+  const { data, error } = await supabase.from("books").insert([book])
+  console.log(book)
+
+  if (error) {
+    console.error("Insert error:", error)
+    console.log("Error: book not inserted")
+  } else {
+    form.reset()
+    console.log("Book added:", data)
+    loadBooks()
+  }
+})
